776e6edf2ca5846b91f03ddf8d25bf59a23184b2
Dennis Nicolas <dnicolas@bu.edu>
2018-04-19 21:06:13 -0400

    Working off a branch, made many updates: Database, Users, started Caching

---
+++ mapproject/map/cache.py
@@ -0,0 +1,33 @@
+from .getArticles import getArticles
+from .models import Article, Collections
+
+basic_ten_articles = []
+
+def cacheArticles(request):
+    if not basic_ten_articles:
+        article_list = getArticles()
+        art_len = len(article_list)
+
+        for i in range(art_len-1):
+            article = create_article(article_list[i], 0)
+            basic_ten_articles.append(article)
+        return article_list
+    else:
+        for i in range(art_len-1):
+            if(Article.objects.get(pk=i) != None):
+                if(Article.objects.get(pk=i).rating == 0):
+                    Article.objects.get(pk=i).delete()
+            else:
+                basic_ten_articles.append(Articles.object.get(pk=i))
+        return article_list
+
+
+def create_article(article, rating):
+    title = article[0]
+    loc = article[1]
+    url = article[2]
+    article = Article.objects.create(title=title,
+                                    location=loc,
+                                    url=url,
+                                    rating=rating)
+    return article
---
+++ mapproject/map/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+# Forms go here
+"""
+class ArticleRating(request):
+    user = request.user
+
+    def star_rating(request, pk):
+        if request.method == 'POST':
+            rating = Article.object.get(pk=pk)
+            user.ratings.add(rating)
+"""
